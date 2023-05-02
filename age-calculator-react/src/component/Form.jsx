b8d2a330744846a1ef53c6ef3afc729f9c0121bb
Khoirul Akmal <khoirulakmal.w@gmail.com>
2023-05-02 19:34:06 +0700

    Passing states based on user input

--- age-calculator-react/src/component/Form.jsx
+++ age-calculator-react/src/component/Form.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 export default function Form(){
+  const [day, setDay] = useState('');
+
+  function handleinput(e){
+    setDay(e.target.value);
+  }
     return(
-<form className="date" id="date" onsubmit="return false">
+<div className="date">
   <label htmlFor="day">DAY</label>
   <label htmlFor="month">MONTH</label>
   <label htmlFor="year">YEAR</label>
-  <input type="number" id="day" name="day" placeholder="DD" min={1} max={31} required />
+  <input type="number" id="day" name="day" placeholder="DD" min={1} max={31} required value={day} onChange={handleinput} />
   <input type="number" id="month" name="month" placeholder="MM" min={1} max={12} required />
   <input type="number" id="year" name="year" placeholder="YYYY" min={1900} max={2022} required />
-  <input type="submit" id="submit-age" />
-</form>
+  <p>{day}</p>
+  <p></p>
+  <p></p>
+</div>
     )
 }
