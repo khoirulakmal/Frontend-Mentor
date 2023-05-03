8ca7bea92fbd327a7195829c6f8afaf9e837a066
Khoirul Akmal <khoirulakmal.w@gmail.com>
2023-05-03 19:58:48 +0700

    Add props for error field

--- age-calculator-react/src/component/Form.jsx
+++ age-calculator-react/src/component/Form.jsx
@@ -2,21 +2,42 @@ import { useState } from "react";
 
 export default function Form(){
   const [day, setDay] = useState('');
+  const [month, setMonth] = useState('');
+  const [year, setYear] = useState('');
+  const [errorDay, setErrorDay] = useState('');
+  const [errorMonth, setErrorMonth] = useState('');
+  const [errorYear, setErrorYear] = useState('');
 
-  function handleinput(e){
+
+  const birthday = day + month + year
+
+  function handleInputDay(e){
     setDay(e.target.value);
   }
+  function handleInputMonth(e){
+    setMonth(e.target.value);
+  }
+  function handleInputYear(e){
+    setYear(e.target.value);
+  }
+  function handleSubmit(e){
+    e.preventDefault();
+    setErrorDay(birthday)
+    setErrorMonth(birthday)
+    setErrorYear(birthday)
+  }
     return(
-<div className="date">
+<form className="date" onSubmit={handleSubmit}>
   <label htmlFor="day">DAY</label>
   <label htmlFor="month">MONTH</label>
   <label htmlFor="year">YEAR</label>
-  <input type="number" id="day" name="day" placeholder="DD" min={1} max={31} required value={day} onChange={handleinput} />
-  <input type="number" id="month" name="month" placeholder="MM" min={1} max={12} required />
-  <input type="number" id="year" name="year" placeholder="YYYY" min={1900} max={2022} required />
-  <p>{day}</p>
-  <p></p>
-  <p></p>
-</div>
+  <input type="number" id="day" name="day" placeholder="DD" min={1} max={31} required value={day} onChange={handleInputDay} />
+  <input type="number" id="month" name="month" placeholder="MM" min={1} max={12} required value={month} onChange={handleInputMonth} />
+  <input type="number" id="year" name="year" placeholder="YYYY" min={1900} max={2022} required value={year} onChange={handleInputYear} />
+  <input type="submit" id="formSubmit"></input>
+  <p>{errorDay}</p>
+  <p>{errorMonth}</p>
+  <p>{errorYear}</p>
+</form>
     )
 }
